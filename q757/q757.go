a626bf75bd70690c060c711a058c58f4c06e4c55
Matty Wang <mattywang7@gmail.com>
2022-07-25 22:16:37 +0800

    q919

---
+++ q757/q757.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	intervals := [][]int{
+		{1, 3},
+		{1, 4},
+		{2, 5},
+		{3, 5},
+	}
+	fmt.Println(intersectionSizeTwo(intervals))
+}
+
+func intersectionSizeTwo(intervals [][]int) (ans int) {
+	sort.Slice(intervals, func(i, j int) bool {
+		a, b := intervals[i], intervals[j]
+		return a[0] < b[0] || a[0] == b[0] && a[1] > b[1]
+	})
+	n, m := len(intervals), 2
+	vals := make([][]int, n)
+	for i := n - 1; i >= 0; i-- {
+		for j, k := intervals[i][0], len(vals[i]); k < m; k++ {
+			ans++
+			for p := i - 1; p >= 0 && intervals[p][1] >= j; p-- {
+				vals[p] = append(vals[p], j)
+			}
+			j++
+		}
+	}
+	return
+}
